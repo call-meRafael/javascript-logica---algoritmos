e833c23f6ec80758ead2b2a88bfb626812da218e
countdown-br <raaphael.araujo@gmail.com>
2026-04-13 18:00:28 -0300

    exercício: programa ordem crescente - gerando elementos visuais básicos e atribuindo classes iniciais.

---
+++ cap06/ordem.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const numeros = [];
+
+// ENUNCIADO:
+// Elabore um programa que adicione números a um array. O programa deve impedir a inclusão de números repetidos. Exibir a lista de números a cada inclusão. Ao clicar no botão 'Verirficar Ordem', o programa deve analisar o conteúdo do array e informar se os números então ou não em ordem crescente.
