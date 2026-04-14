f8922397da7d5821f3169b8919524765fc4ff246
countdown-br <raaphael.araujo@gmail.com>
2026-04-13 22:36:37 -0300

    programa-ordem-crescente; etapa 2, inserindo a lógica

--- cap06/ordem.js
+++ cap06/ordem.js
@@ -1,6 +1,46 @@
-'use strict';
+"use strict";
 
-const numeros = [];
+const frm = document.querySelector("form");
+const getNumber = document.querySelector("#inNumero");
+const listaNum = document.querySelector('.nmbr-lst');
+const alertMsg = document.querySelector('.alert-msg1');
 
 // ENUNCIADO:
 // Elabore um programa que adicione números a um array. O programa deve impedir a inclusão de números repetidos. Exibir a lista de números a cada inclusão. Ao clicar no botão 'Verirficar Ordem', o programa deve analisar o conteúdo do array e informar se os números então ou não em ordem crescente.
+
+const numeros = [];
+
+const handleAdd = (numero) => {
+  
+  
+
+  const valorNum = getNumber.value;
+
+  if (numeros.includes(valorNum)) {
+
+    return alertMsg.textContent = `O número ${valorNum} já existe na lista!`;
+
+    
+  }  else {
+    numeros.push(valorNum);
+  }
+
+  
+
+  for (let num = 0; num < numeros.length; num++) {
+    // Exibe a lista de números na tela a cada inclusão.
+    listaNum.innerHTML += `<li>${numeros[num]}</li>`;
+
+  }
+
+  
+};
+
+frm.addEventListener('submit', handleAdd);
+console.log(handleAdd(numeros));
+
+// for (let num = 0; num < numeros.length; num++) {
+//   if (numeros[num] > numeros[num + 1]) {
+//     return "Os números não estão em ordem crescente!";
+//   }
+// }
