e45036cc8e1a26baabb28f0152b25ecdeae1aab6
countdown-br <raaphael.araujo@gmail.com>
2026-04-16 11:36:42 -0300

    programa-concurso: estilo aprimorado para melhor legibilidade do usuário; lógica inicial  de entrada refatorada e mais robusta

--- cap06/app-concurso.js
+++ cap06/app-concurso.js
@@ -7,6 +7,7 @@
 const frm = document.querySelector("form");
 const getName = document.querySelector("#inCandidato");
 const getNumbr = document.querySelector("#inAcertos");
+const invalidState = document.querySelector('.invalid-state');
 
 const aprovados = [];
 
@@ -21,23 +22,44 @@ const formatNome = (nome) => {
   return candidato;
 };
 
-
+// Função responsável por colher os dados do formulário e adicionar o candidato à lista.
 const getData = (event) => {
   event.preventDefault();
-
-  const usuario = formatNome(getName.value);
-
-  if (getName.value.length < 3 || getName.value.length > 15) {
+  
+  const nome = getName.value;
+  const usuario = formatNome(nome);
+  const acertos = Number(getNumbr.value);
+  
+  
+  if (usuario.length < 3 || usuario.length > 15) {
+    event.target.reset();// Segundo reset para limpar entrada pós erro.
     return alert('O nome do candidato deve conter entre 3 e 15 caracteres!');
   }
+  
+  if (getNumbr.value < 0) {
+    return alert('O número de acertos não pode ser negativo!');
+  }
+  
+  aprovados.push({ candidato: usuario, acertos: acertos });
+  console.log(aprovados);
 
-  formatNome(getName.value);
-  aprovados.push(usuario);
-
-  event.target.reset();
+  event.target.reset(); // Limpa o formulário.
 };
 
+// Ouvinte do evento de envio do formulário.
 frm.addEventListener("submit", getData);
-console.log(aprovados);
+
+// Checa se o nome do candidato é válido impedindo a inserção de caracteres não numéricos.
+getName.addEventListener('input', () => {
+  if (/\d/.test(getName.value)) {
+    getName.classList.add('error-state');
+    getName.style.outline = 'none';
+    invalidState.classList.remove('no-display');
+  } else {
+    getName.classList.remove('error-state');
+    invalidState.classList.add('no-display');
+  }
+})
+
 
 
