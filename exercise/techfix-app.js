b364a52b844197592df1685b826356850d7e60d2
countdown-br <raaphael.araujo@gmail.com>
2026-04-24 14:44:32 -0300

    refatorando a parte lógica do exercício

--- exercise/techfix-app.js
+++ exercise/techfix-app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Você terá um único array principal chamado ordensDeServico. Cada objeto dentro dele deve ter: id (gerado dinamicamente), cliente, aparelho, servico, valor, e status (que começa como "Pendente").
+
 const frm = document.querySelector("form");
 const inUser = document.querySelector("#inCliente");
 const inputValueOrder = document.querySelector(".order-value");
@@ -8,37 +10,7 @@ const totalIncome = document.querySelector(".total-income-value");
 
 const carrinho = [];
 
-const ordensDeServico = [
-  { id: userId },
-  { cliente: inUser.value },
-  { aparelho: inAparelho.value },
-  {
-    servico: {
-      id: 0,
-      ordem: "Selecione um serviço",
-      valor: 0.0,
-      id: 1,
-      ordem: "Limpeza Básica",
-      valor: 50.0,
-      id: 2,
-      ordem: "Limpeza Profunda",
-      valor: 100.0,
-      id: 3,
-      ordem: "Troca de Analógico",
-      valor: 150.0,
-      id: 4,
-      ordem: "Reparo de Placa",
-      valor: 200.0,
-      id: 5,
-      ordem: "Calibragem de Sensor",
-      valor: 250.0,
-      id: 6,
-      ordem: "Manutenção de Televisor",
-      valor: 300.0,
-    },
-  },
-  { status: "Pendente" },
-];
+const ordensDeServico = 
 
 const formatador = Intl.NumberFormat("pt-BR", {
   style: "currency",
@@ -47,18 +19,6 @@ const formatador = Intl.NumberFormat("pt-BR", {
   maximumFractionDigits: 2,
 });
 
-const handleOrderValue = (id) => {
-  const order = ordensDeServico[2].servico.find((order) => order.id === Number(id));
-  return order ? formatador.format(order.value) : "R$ 0,00";
-};
-
-selectOrder.addEventListener("change", (event) => {
-  const id = event.target.value;
-  inputValueOrder.innerHTML = handleOrderValue(id);
-  
-//   inputValueOrder.innerHTML = handleOrderValue(event.target.value);
-});
-inputValueOrder.innerHTML = handleOrderValue(selectOrder.value);
 
 const formatName = (name) => {
   return name
@@ -68,14 +28,3 @@ const formatName = (name) => {
     .map((char) => char.charAt(0).toUpperCase() + char.slice(1))
     .join(" ");
 };
-console.log(carrinho);
-
-frm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const cliente = formatName(ordensDeServico[1].cliente);
-
-  inUser.focus();
-  event.target.reset();
-  console.log(carrinho);
-});
